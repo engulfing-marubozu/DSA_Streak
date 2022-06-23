75c00931b523432b266baba8cf599751307ae21e
Rahul Raj <2019ume1141@mnit.ac.in>
2022-06-23 05:55:02 +0530

    knapsack

--- knapsackrec.cpp
+++ knapsackrec.cpp
@@ -3,12 +3,15 @@
 #define ll long long int;
 using namespace std;
 int rupee[]={0,4,5,3,2}, mango_utilities[]={0,8,2,5,9};
+int mem[5][5]={0};
 
 
 int arr(int i, int j)
 {   
     if(i==0||j==0)
      return 0;
+    if(mem[i][j]!=0)
+      return mem[i][j]; 
 //     if(j<0)
 //       return INT16_MIN; 
     
@@ -17,7 +20,7 @@ int arr(int i, int j)
              x =arr(i-1,j);
             else
              x=max(arr(i-1,j) , mango_utilities[i]+arr(i-1,j-rupee[i]) );
-
+    mem[i][j]= x;
     return x;
 }
 
