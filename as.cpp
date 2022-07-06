d53949ca8970942bce525e7feb3833cc798dc21d
Rahul Raj <2019ume1141@mnit.ac.in>
2022-07-06 13:01:52 +0530

    list c==

--- as.cpp
+++ as.cpp
@@ -2,7 +2,7 @@
 #define ll long long int 
 using namespace std;
 int main()
-{  vector<int> vect{10,45,54};
+{  vector<int> vect{10,-45,54};
  all_of(vect.begin(), vect.end(), [](int x) { return x>0; })?
           cout << "All are positive elements" :
           cout << "All are not positive elements";
